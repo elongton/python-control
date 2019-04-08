02544615cf5691794794fb7da0234cceec178e4d
elongton <elongton@vt.edu>
2019-04-08 00:58:28 -0400

    updated lqr_servo example

--- lqr_servo_pendulum.py
+++ lqr_servo_pendulum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 import control as c
 import matplotlib.pyplot as plt
 #Define system matrices
@@ -21,9 +22,30 @@ Q = np.transpose(Cz)*Rzz*Cz
 
 K, S, E = c.lqr(A, Bu, Q, R)
 
-t = np.arange(0, 10, 0.1)
-#r1 = [0.1*np.sin(0.9*np.sqrt(g/l)*np.transpose(t)), np.zeros(len(t), 1)];
-r11 = 0.1*np.sin(0.9*np.sqrt(g/l)*np.transpose(t))
-#print(r11)
+t = np.arange(0, 10, 0.01)
+r1 = np.c_[0.1*np.sin(0.9*np.sqrt(g/l)*t), np.zeros([len(t), 1])]
+r2 = np.c_[0.1*np.sin(0.9*np.sqrt(g/l)*t), (0.9*np.sqrt(g/l))*0.1*np.cos(0.9*np.sqrt(g/l)*t)]
 
-plt.plot(t, r11)
+#Create an output vector: position, velocity, control
+Cy = np.array([[1,0],[0,1],-K[0]]);
+Dy = np.r_[np.zeros([2,2]), [K[0]]]
+
+sys = signal.StateSpace(A-Bu*K, Bu*K, Cy, Dy);
+tout1, y1, x1 = signal.lsim(sys, r1, t)
+tout2, y2, x2 = signal.lsim(sys, r2, t)
+#print(x1[:,1])
+
+plt.figure()
+plt.plot(t, y1[:,0])
+plt.plot(t, y2[:,0])
+plt.plot(t, r2[:,0])
+
+plt.figure()
+plt.plot(t, y1[:,1])
+plt.plot(t, y2[:,1])
+plt.plot(t, r1[:,1])
+plt.plot(t, r2[:,1])
+
+plt.figure()
+plt.plot(t, y1[:,2])
+plt.plot(t, y2[:,2])
